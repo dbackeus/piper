12b3a53946e441b2da6736755412c6530a4a2a08
David Backeus <duztdruid@gmail.com>
2009-04-09 13:06:43 +0200

    Removed unneccessary rails modules like active record to avoid having to have a database

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,8 @@ require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
 
+  config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
+
   config.gem "httparty"
 
   config.time_zone = 'UTC'
